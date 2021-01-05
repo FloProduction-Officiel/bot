1a7070139f3e860b79ca8e4fdd11779c7763555f
samiFBG <62943665+samiFBG@users.noreply.github.com>
2021-01-05 17:11:08 +0100

    initial commit

---
+++ inex.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const {prefix, token} = require('./config.json');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+client.on('message',message =>{
+    if (message.content === `${prefix} ping`) {
+		message.channel.send('Pong.');
+    }
+});
+
+client.login(token);
