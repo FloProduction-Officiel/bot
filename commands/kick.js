87f2a59d211f484b6733cfc265da577c5dcc9257
samsvr75 <samsvr75@gmail.com>
2021-01-10 21:17:53 +0100

    update kick function

--- commands/kick.js
+++ commands/kick.js
@@ -1,15 +1,27 @@
 module.exports = {
 	name: 'kick',
-	description: 'kick!',
+    description: 'kick!',
 	execute(message, args) {  
         if (!message.mentions.users.size) {
             return message.reply('tu dois tagger un utilisateur pour le kick');
         }
-        if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("Tu as as les permission de kick un membre")
-         const taggedUser = message.mentions.users.first();
-         let kickReason = args.join(" ").slice(22);
-         let author = message.author ; 
-         message.channel.send(`tu veus kick ${taggedUser} avec la raison ${kickReason}`);
-        taggedUser.send(`tu as etait kick par ${author} pour la raison ${kickReason}` )
-	}
-};
+        if (!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("Tu n'as pas les permission de kick un membre")
+            const author = message.author ;     
+            let kickReason = args.join(" ").slice(22);
+            const taguser = message.mentions.users.first();  
+            if (author == taguser)return message.reply('tu peux pas te kick toi même') ;
+            if (taguser) {  
+                const tagmember = message.guild.member(taguser);         
+                if (tagmember) { 
+                    setTimeout(async function(){ 
+                        await tagmember.send(`tu as été kick par ${author}  pour  ${kickReason}` )
+                        await tagmember
+                        .kick('Optional reason that will display in the audit logs')
+                        message.channel.send(` ${taguser} à été kick `);    
+                    }, 100);
+                }else{
+                message.reply("That user isn't in this guild!");
+            }
+        };
+    }
+}
