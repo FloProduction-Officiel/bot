8e1fb88be03664b6a6d2c9f6291590509f35b948
samsvr75 <samsvr75@gmail.com>
2021-01-11 20:18:40 +0100

    horoku prefix

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 client.on('message',message =>{
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    if (!message.content.startsWith(process.env.prefix) || message.author.bot) return;
 
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const command = args.shift().toLowerCase();
